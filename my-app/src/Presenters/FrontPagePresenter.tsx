3160f59083a340641a733e8b420946f0efe883ab
Deni Persson <deni.persson@gmail.com>
2023-03-13 10:53:21 +0100

    Variable renaming

--- my-app/src/Presenters/FrontPagePresenter.tsx
+++ my-app/src/Presenters/FrontPagePresenter.tsx
@@ -5,8 +5,8 @@ export default function FrontPagePresenter(){
     function processText(text:string){
         const words = text.split(' ');
         const boldWords = words.map(word => {
-            const first30Percent = Math.ceil(word.length * 0.5);
-            const boldedText = `<strong>${word.slice(0, first30Percent)}</strong>${word.slice(first30Percent)}`;
+            const amountToBolden = Math.ceil(word.length * 0.5);
+            const boldedText = `<strong>${word.slice(0, amountToBolden)}</strong>${word.slice(amountToBolden)}`;
             return boldedText;
         });
         return boldWords.join(' ');
